855d5249d07b26a2dab27c4033d0fd9128652974
Jannis Pohlmann <contact@jannispohlmann.de>
2017-02-02 14:29:26 +0100

    Add a TextEditor plugin for JS syntax highlighting and editing

---
+++ src/utils/EditorPlugins/JSEditorPlugin.js
@@ -0,0 +1,139 @@
+/* @flow */
+import React from 'react'
+import Slate from 'slate'
+import Theme from 'js-theme'
+import { Colors } from '@workflo/styles'
+
+const acorn = require('acorn/dist/acorn_loose')
+
+/**
+ * Themeing
+ */
+
+const defaultTheme = {
+  identifier: {
+    color: 'blue',
+  },
+  keyword: {
+    color: 'green'
+  }
+}
+
+type DecoratorPropsT = {
+  theme: Object,
+  children: React.Children,
+  mark: Slate.Mark,
+}
+
+/**
+ * Mark implementations
+ */
+
+const Identifier = ({ children, theme }) => (
+  <span {...theme.identifier}>
+    {children}
+  </span>
+)
+
+const ThemedIdentifier = Theme('Identifier', defaultTheme)(Identifier)
+
+const Keyword = ({ children, theme }) => (
+  <span {...theme.keyword}>
+    {children}
+  </span>
+)
+
+const ThemedKeyword = Theme('Keyword', defaultTheme)(Keyword)
+
+/**
+ * Decorators
+ */
+
+const decorateIdentifiers = (characters, ast, options) => {
+  const declarations = ast.body
+    .filter((node) => node.type === 'VariableDeclaration')
+    .map((node) => ({
+      start: node.declarations[0].id.start,
+      end: node.declarations[0].id.end,
+    }))
+
+  return characters.map((char, index) =>
+    declarations.reduce((char, declaration) => {
+      if (index < declaration.start || index >= declaration.end) {
+        return char
+      } else {
+        const mark = Slate.Mark.create({
+          type: 'identifier'
+        })
+        return char.merge({
+          marks: char.marks.add(mark)
+        })
+      }
+    }, char)
+  )
+}
+
+const decorateKeywords = (characters, ast, options) => {
+  const declarations = ast.body
+    .filter((node) => node.type === 'VariableDeclaration')
+    .map((node) => ({
+      start: node.start,
+      end: node.start + (node.kind ? node.kind.length : 0)
+    }))
+
+  return characters.map((char, index) =>
+    declarations.reduce((char, declaration) => {
+      if (index < declaration.start || index >= declaration.end) {
+        return char
+      } else {
+        const mark = Slate.Mark.create({
+          type: 'keyword'
+        })
+        return char.merge({
+          marks: char.marks.add(mark)
+        })
+      }
+    }, char)
+  )
+}
+
+const combineDecorators = (decorators: Array<Function>, options) => {
+  return (text: Slate.Text, block: Slate.Block) => {
+    try {
+      const ast = acorn.parse_dammit(text.text)
+      return decorators.reduce((characters, decorator) => {
+        return decorator(characters, ast, options)
+      }, text.characters)
+    } catch (error) {
+      return text.characters
+    }
+  }
+}
+
+/**
+ * CodeEditorPlugin
+ */
+
+type PluginOptions = {
+}
+
+const JSEditorPlugin = (options : PluginOptions) => {
+  return {
+    schema: {
+      nodes: {
+        code: {
+          decorate: combineDecorators([
+            decorateIdentifiers,
+            decorateKeywords,
+          ], options),
+        }
+      },
+      marks: {
+        'identifier': ThemedIdentifier,
+        'keyword': ThemedKeyword,
+      },
+    }
+  }
+}
+
+export default JSEditorPlugin
