ca9dacd350a81f7204de2da4c3617132c318d031
Jannis Pohlmann <contact@jannispohlmann.de>
2017-02-02 14:27:13 +0100

    Add a dedicated PropKeyValueT type

---
+++ src/types/PropKeyValueT.js
@@ -0,0 +1,28 @@
+/* @flow */
+
+export type PropKeyValueTypeT =
+  'Radio' |
+  'Checkbox' |
+  'Slider' |
+  'TextInput' |
+  'Date' |
+  'Color' |
+  'Icon'
+
+type InputT = {
+  type: PropKeyValueTypeT,
+}
+
+export type ValueTypeT = 'String' | 'Javascript'
+
+type ValueT = {
+  type: ValueTypeT,
+  value: any,
+}
+
+export type PropKeyValueT = {
+  key: string,
+  value: ValueT,
+  options: Array<string>,
+  input: InputT,
+}
