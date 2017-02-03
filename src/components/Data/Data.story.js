0d98bd0f0925f219ade9340bc16a3e7a8b7328a9
Jannis Pohlmann <contact@jannispohlmann.de>
2017-02-03 14:36:05 +0100

    Add separate stories for Data with the actions and data plugins

--- src/components/Data/Data.story.js
+++ src/components/Data/Data.story.js
@@ -4,42 +4,61 @@ import Data from './Data'
 import PreviewContainer from '@workflo/components/lib/PreviewContainer/PreviewContainer'
 import Preview from '@workflo/components/lib/Preview'
 import actionsBabelPlugin from '../../utils/ActionsBabelPlugin'
+import dataBabelPlugin from '../../utils/DataBabelPlugin'
 
 storiesOf('Data', module)
-  .add('Regular', () => (
+  .add('Data plugin', () => (
     <PreviewContainer
       shade='light'
     >
       <Preview
         label='Data'
       >
-        <DataContainer />
+        <DataContainer
+          text={initialDataText}
+          plugin={dataBabelPlugin}
+        />
       </Preview>
     </PreviewContainer>
   ))
-const babelOptions = {
-    presets: ['es2015', 'react', 'stage-0'],
-    plugins: [actionsBabelPlugin],
-    filename: 'workflo',
-    babelrc: false,
-  }
+  .add('Actions plugin', () => (
+    <PreviewContainer
+      shade='light'
+    >
+      <Preview
+        label='Data'
+      >
+        <DataContainer
+          text={initialActionsText}
+          plugin={actionsBabelPlugin}
+        />
+      </Preview>
+    </PreviewContainer>
+  ))
 
 class DataContainer extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      text: initialText,
+      text: this.props.text,
     }
   }
 
   handleChange = (text) => {
     action('onChange')(text)
+
+    const babelOptions = {
+      presets: ['es2015', 'react', 'stage-0'],
+      plugins: [this.props.plugin],
+      filename: 'workflo',
+      babelrc: false,
+    }
+
     try {
       const es5Ast = Babel.transform(text, babelOptions)
-      console.log('code: ', es5Ast.code)
-      // eval(es5Ast.code)
+      console.log('code', es5Ast.code)
     } catch (err) {
-      console.error(err.message)
+      console.error(err)
     }
   }
 
@@ -53,4 +72,12 @@ class DataContainer extends React.Component {
   }
 }
 
-const initialText = ``
+const initialDataText = `
+const button = ({title}) => {}
+const label = ({caption}) => {}
+`
+
+const initialActionsText = `
+const handleClick = ({event}) => {}
+const handleHover = ({event}) => {}
+`
