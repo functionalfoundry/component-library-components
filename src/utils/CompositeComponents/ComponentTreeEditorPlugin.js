5f489a69a5be0b4f1351efdfbb643a9b601c60c5
Jannis Pohlmann <contact@jannispohlmann.de>
2017-02-14 20:08:57 +0100

    WIP: Add a ComponentTreeEditorPlugin skeleton

---
+++ src/utils/CompositeComponents/ComponentTreeEditorPlugin.js
@@ -0,0 +1,33 @@
+/* @flow */
+
+import React from 'react'
+import Theme from 'js-theme'
+import { Colors } from '@workflo/styles'
+
+const Slate = require('slate')
+
+import { ComponentTree } from './ComponentTree'
+import ComponentTreeUtils from './ComponentTreeUtils'
+
+/**
+ * Plugin options
+ */
+
+type PluginOptionsT = {
+  tree: ComponentTree,
+}
+
+/**
+ * Mark renderers
+ */
+
+/**
+ * ComponentTreeEditorPlugin implementation
+ */
+
+const ComponentTreeEditorPlugin = (options: PluginOptionsT) => ({
+  schema: {
+  }
+})
+
+export default ComponentTreeEditorPlugin
