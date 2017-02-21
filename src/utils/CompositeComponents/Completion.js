2702724e8736498d2caa41555a4ec1bfb3c262c4
Jannis Pohlmann <contact@jannispohlmann.de>
2017-02-21 15:35:55 +0100

    Add data types to represent component and prop completion data

---
+++ src/utils/CompositeComponents/Completion.js
@@ -0,0 +1,37 @@
+/* @flow */
+
+import type { PropValueTypeT } from './ComponentTree'
+
+/**
+ * Completion data types
+ */
+
+export type PropCompletionDataT = {
+  type: PropValueTypeT,
+  options: Array<any>,
+}
+
+export type PropsCompletionDataT = {
+  [key:string]: PropCompletionDataT,
+}
+
+export type OptionSourceT = 'state' | 'data' | 'actions'
+
+export type GlobalOptionDataT = {
+  type: PropValueTypeT,
+  source: OptionSourceT,
+}
+
+export type GlobalOptionsDataT = {
+  [key:string]: GlobalOptionDataT,
+}
+
+export type ComponentPropsCompletionDataT = {
+  [key:string]: PropsCompletionDataT
+}
+
+export type CompletionDataT = {
+  components: Array<string>,
+  props: ComponentPropsCompletionDataT,
+  globalOptions: GlobalOptionsDataT,
+}
