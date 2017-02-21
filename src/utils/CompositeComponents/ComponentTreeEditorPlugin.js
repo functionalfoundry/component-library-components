65e89294bca1c7a2fcf2e23934f8cc1cb9631b60
Jannis Pohlmann <contact@jannispohlmann.de>
2017-02-21 15:40:13 +0100

    Make prop names, values and component texts (halfway) editable
    For prop names, we're simply rendering a clickable minus symbol like
    before. For values, we're rendering an on-click popup with a
    property-type-specific value chooser. For component texts we are
    temporarily rendering a clickable minus symbol, although this is
    mainly as a proof of concept.
    
    All actions result in a new component tree that is passed back up
    to the ComponentTreeEditor via the plugin's onChange function prop.

--- src/utils/CompositeComponents/ComponentTreeEditorPlugin.js
+++ src/utils/CompositeComponents/ComponentTreeEditorPlugin.js
@@ -2,11 +2,21 @@
 
 import React from 'react'
 import Theme from 'js-theme'
+import { Set } from 'immutable'
+import { Popover, Trigger, View } from '@workflo/components'
 import { Colors } from '@workflo/styles'
+import {
+  AnyPropValueChooser,
+} from '../../components/ComponentTreeEditor/PropValueChoosers'
+import { ComponentTree, Prop, PropValue } from './ComponentTree'
+import type {
+  CompletionDataT,
+  GlobalOptionsDataT,
+  PropCompletionDataT,
+} from './Completion'
 
 const Slate = require('slate')
-
-import { ComponentTree } from './ComponentTree'
+const Utils = require('./ComponentTreeUtils')
 
 /**
  * Plugin options
@@ -14,11 +24,267 @@ import { ComponentTree } from './ComponentTree'
 
 type PluginOptionsT = {
   tree: ComponentTree,
+  completionData?: CompletionDataT,
+  onChange?: Function,
 }
 
 /**
  * Mark renderers
  */
+
+const defaultTheme = {
+  propName: {
+    position: 'relative',
+    color: '#009e71',
+  },
+  propRemover: {
+    cursor: 'pointer',
+    top: 0,
+    left: '-35px',
+    width: 35,
+    height: '100%',
+    display: 'inline-block',
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  propValueChooser: {
+    display: 'inline',
+    cursor: 'pointer',
+    ':hover': {
+      background: Colors.grey200,
+    }
+  },
+  text: {
+    position: 'relative',
+  },
+  textRemover: {
+    cursor: 'pointer',
+    top: 0,
+    left: '-35px',
+    width: 35,
+    height: '100%',
+    display: 'inline-block',
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}
+
+type MarkRendererPropsT = {
+  children: React.Children,
+  mark: Slate.Mark,
+  marks: Set<Slate.Mark>,
+  options: PluginOptionsT,
+  theme: any,
+}
+
+/**
+ * Property remover
+ */
+
+type PropNameRendererStateT = {
+  isShowingMinus: boolean,
+}
+
+class PropNameRenderer extends React.Component {
+  props: MarkRendererPropsT
+  state: PropNameRendererStateT
+
+  constructor (props) {
+    super(props)
+    this.state = { isShowingMinus: false }
+  }
+
+  render () {
+    const { children, theme } = this.props
+    const { isShowingMinus } = this.state
+    return (
+      <View
+        {...theme.propName}
+        inline
+      >
+        <Trigger
+          triggerOn={['Hover', 'Mouse leave']}
+          onTrigger={this.handleTrigger}
+        >
+          <View
+            {...theme.propRemover}
+            inline
+          >
+            {isShowingMinus &&
+              <View onClick={this.handleClick} inline>
+                {'-'}
+              </View>}
+          </View>
+        </Trigger>
+        {children}
+      </View>
+    )
+  }
+
+  handleTrigger = () => {
+    this.setState({ isShowingMinus: !this.state.isShowingMinus })
+  }
+
+  handleClick = () => {
+    const { mark, options } = this.props
+    const prop = mark.getIn(['data', 'element', 'data', 'prop'])
+    const tree = Utils.removeProp(options.tree, prop.id)
+    options.onChange && options.onChange(tree)
+  }
+}
+
+const ThemedPropNameRenderer =
+  Theme('PropNameRenderer', defaultTheme)(PropNameRenderer)
+
+/**
+ * Property value chooser
+ */
+
+const propValueChooserImplementations = {
+  'any': AnyPropValueChooser,
+}
+
+const getPropValueRenderer = (
+  prop: Prop,
+  propCompletionData: PropCompletionDataT
+) => {
+  if (propCompletionData) {
+    return propValueChooserImplementations[propCompletionData.type]
+        || propValueChooserImplementations['any']
+  } else {
+    return propValueChooserImplementations['any']
+  }
+}
+
+class PropValueRenderer extends React.Component {
+  props: MarkRendererPropsT
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    const { children, mark, marks, options, theme } = this.props
+    const prop = mark.getIn(['data', 'element', 'data', 'prop'])
+    const component = mark.getIn(['data', 'element', 'data', 'component'])
+    const value = mark.getIn(['data', 'element', 'node'])
+
+    const propCompletionData =
+      options
+      && options.completionData
+      && options.completionData.props
+      && options.completionData.props[component.name]
+      && options.completionData.props[component.name][prop.name]
+
+    const globalOptions =
+      options
+      && options.completionData
+      && options.completionData.globalOptions
+
+    const Chooser = getPropValueRenderer(prop, propCompletionData)
+
+    return (
+      <View
+        {...theme.propValueChooser}
+        inline
+      >
+        <Popover
+          position='Right'
+          horizontalOffset={5}
+          verticalOffset={2}
+          portal={(
+            <Chooser
+              value={value}
+              completionData={propCompletionData}
+              options={globalOptions}
+              onChange={this.handleChange}
+            />
+          )}
+        >
+          <span>
+            {children}
+          </span>
+        </Popover>
+      </View>
+    )
+  }
+
+  handleChange = (value) => {
+    const { mark, options } = this.props
+    const prop = mark.getIn(['data', 'element', 'data', 'prop'])
+    const propValue = mark.getIn(['data', 'element', 'node'])
+    const tree = Utils.setPropValue(
+      options.tree, prop.id, propValue.set('value', value)
+    )
+    options.onChange && options.onChange(tree)
+  }
+}
+
+const ThemedPropValueRenderer =
+  Theme('PropValueRenderer', defaultTheme)(PropValueRenderer)
+
+/**
+ * Component text editor
+ */
+
+type TextRendererStateT = {
+  isShowingMinus: boolean,
+}
+
+class TextRenderer extends React.Component {
+  props: MarkRendererPropsT
+  state: TextRendererStateT
+
+  constructor (props) {
+    super(props)
+    this.state = { isShowingMinus: false }
+  }
+
+  render () {
+    const { children, theme } = this.props
+    const { isShowingMinus } = this.state
+    return (
+      <View
+        {...theme.text}
+        inline
+      >
+        <Trigger
+          triggerOn={['Hover', 'Mouse leave']}
+          onTrigger={this.handleTrigger}
+        >
+          <View
+            {...theme.textRemover}
+            inline
+          >
+            {isShowingMinus &&
+              <View onClick={this.handleClick} inline>
+                {'-'}
+              </View>}
+          </View>
+        </Trigger>
+        {children}
+      </View>
+    )
+  }
+
+  handleTrigger = () => {
+    this.setState({ isShowingMinus: !this.state.isShowingMinus })
+  }
+
+  handleClick = () => {
+    const { mark, options } = this.props
+    const component = mark.getIn(['data', 'element', 'node'])
+    const tree = Utils.setComponentText(options.tree, component.id, null)
+    options.onChange && options.onChange(tree)
+  }
+}
+
+const ThemedTextRenderer =
+  Theme('TextRenderer', defaultTheme)(TextRenderer)
 
 /**
  * ComponentTreeEditorPlugin implementation
@@ -26,7 +292,27 @@ type PluginOptionsT = {
 
 const ComponentTreeEditorPlugin = (options: PluginOptionsT) => ({
   schema: {
-  }
+    marks: {
+      'prop-name': (props: Object) => (
+        <ThemedPropNameRenderer
+          {...props}
+          options={options}
+        />
+      ),
+      'prop-value': (props: Object) => (
+        <ThemedPropValueRenderer
+          {...props}
+          options={options}
+        />
+      ),
+      'text': (props: Object) => (
+        <ThemedTextRenderer
+          {...props}
+          options={options}
+        />
+      )
+    },
+  },
 })
 
 export default ComponentTreeEditorPlugin
