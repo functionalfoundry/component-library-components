b8322c23b17ecff90a9e30d842d1a31284dacedd
Jannis Pohlmann <contact@jannispohlmann.de>
2017-02-21 17:28:53 +0100

    Log onChange actions in the ComponentTreeEditor story

--- src/components/ComponentTreeEditor/ComponentTreeEditor.story.js
+++ src/components/ComponentTreeEditor/ComponentTreeEditor.story.js
@@ -248,6 +248,7 @@ class TreeEditorContainer extends React.Component {
         <ComponentTreeEditor
           tree={this.state.tree}
           completionData={completionData}
+          onChange={action('onChange')}
         />
         <div>
           {[
