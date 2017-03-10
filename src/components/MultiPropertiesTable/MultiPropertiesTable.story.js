881023df633f796c57b5f4a13030ef70ba31eed1
Yaniv Tal <yanivtalmusic@gmail.com>
2017-03-10 14:48:18 -0800

    Add MultiPropertiesTable

---
+++ src/components/MultiPropertiesTable/MultiPropertiesTable.story.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { storiesOf, action } from '@kadira/storybook'
+import Preview from '@workflo/components/lib/Preview'
+import PreviewContainer from '@workflo/components/lib/PreviewContainer/PreviewContainer'
+import MultiPropertiesTable from './MultiPropertiesTable'
+
+storiesOf('MultiPropertiesTable', module)
+  .add('Regular', () => (
+    <PreviewContainer>
+      <Preview
+        label='Regular'
+      >
+        <MultiPropertiesTable
+          components={components}
+          onClickPlus={action('onClickPlus')}
+          onClickMinus={action('onClickMinus')}
+          onChangeComponent={action('onChangeComponent')}
+        />
+      </Preview>
+    </PreviewContainer>
+  ))
+
+const components = [
+  {
+    name: 'Tab',
+    id: '1',
+    properties: [
+      {
+        name: 'children',
+        type: 'React.Element',
+        default: '',
+        description: 'The content of the tab label',
+        isUsedByTreeEditor: false,
+      }
+    ],
+  },
+  {
+    name: 'Tabs',
+    id: '2',
+    properties: [
+      {
+        name: 'kind',
+        type: 'Primary | Secondary',
+        default: 'false',
+        description: 'The primary kind uses the underline and secondary uses a colored background',
+        isUsedByTreeEditor: true,
+      }
+    ],
+  },
+  {
+    name: 'TabList',
+    id: '3',
+    properties: [
+    ],
+  },
+]
