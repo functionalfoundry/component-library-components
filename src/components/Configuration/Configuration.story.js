cf871f9314361324d4c43819d61dc7123aea4887
Yaniv Tal <yanivtalmusic@gmail.com>
2017-03-30 21:39:38 -0700

    Add configuration component for repos and themes

--- src/components/Configuration/Configuration.story.js
+++ src/components/Configuration/Configuration.story.js
@@ -11,13 +11,49 @@ storiesOf('Configuration', module)
         label='Regular'
         theme={{
           preview: {
-            width: 300,
+            width: 900,
           },
         }}
       >
         <Configuration
           onChange={action('onChange')}
+          onClickAddRepo={action('onClickAddRepo')}
+          onClickAddTheme={action('onClickAddTheme')}
+          onClickDeleteRepo={action('onClickDeleteRepo')}
+          onClickDeleteTheme={action('onClickDeleteTheme')}
+          configuration={{
+            repos,
+            themes,
+          }}
         />
       </Preview>
     </PreviewContainer>
   ))
+
+const repos = [
+  {
+    name: 'Example Library',
+    rootDirectory: '/User/Projects/workflo/example-library',
+    sourcePath: 'src',
+    filePattern: '/**/index.js',
+    namePattern: '/()/index.js',
+  },
+  {
+    name: 'Components',
+    rootDirectory: '/User/Projects/workflo/components',
+    sourcePath: 'src',
+    filePattern: '/**/index.js',
+    namePattern: '/()/index.js',
+  },
+]
+
+const themes = [
+  {
+    name: 'Light',
+    backgroundColor: '#fff',
+  },
+  {
+    name: 'Dark',
+    backgroundColor: '#434345',
+  },
+]
