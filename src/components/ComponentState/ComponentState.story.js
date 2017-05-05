39f888738fc650ce00bcbfe33e9ca661791cbd86
Jannis Pohlmann <contact@jannispohlmann.de>
2017-05-05 17:29:11 +0200

    ComponentState story: Format with prettier

--- src/components/ComponentState/ComponentState.story.js
+++ src/components/ComponentState/ComponentState.story.js
@@ -2,13 +2,10 @@ import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
 import ComponentState from './ComponentState'
 import QuickAction from '../QuickAction'
-import {
-  Preview,
-  PreviewContainer,
-} from '@workflo/components'
+import { Preview, PreviewContainer } from '@workflo/components'
 
-const MyComponent = ({children}) => (
-  <div style={{ backgroundColor: 'magenta', width: 100, height: 100, }}>
+const MyComponent = ({ children }) => (
+  <div style={{ backgroundColor: 'magenta', width: 100, height: 100 }}>
     {children}
   </div>
 )
@@ -21,108 +18,95 @@ const element = (
 
 const actions = [
   <QuickAction
-    icon='delete'
+    icon="delete"
     input={{
       type: 'Radio',
-      options: [
-        'Left',
-        'Center',
-        'Right',
-      ],
+      options: ['Left', 'Center', 'Right'],
       value: 'Center',
     }}
     onChange={action('Alignment')}
   />,
   <QuickAction
-    icon='theme'
-    label='Theme'
+    icon="theme"
+    label="Theme"
     input={{
-      options: [
-        'Light',
-        'Dark',
-        'Grey',
-      ],
+      options: ['Light', 'Dark', 'Grey'],
       value: 'Dark',
       type: 'Radio',
     }}
   />,
   <QuickAction
-    icon='duplicate'
+    icon="duplicate"
     input={{
       type: 'Radio',
-      options: [
-        'Small',
-        'Base',
-        'Large',
-      ],
+      options: ['Small', 'Base', 'Large'],
       value: 'Base',
     }}
     onChange={action('Size')}
   />,
 ]
 
-storiesOf('Component State', module)
-  .add('Regular', () => (
-    <PreviewContainer>
-      <Preview
-        label='Regular'
-        theme={{
-          content: {
-            margin: 60,
-          },
-        }}
-      >
-        <Container />
-      </Preview>
-    </PreviewContainer>
-  ))
+storiesOf('Component State', module).add('Regular', () => (
+  <PreviewContainer>
+    <Preview
+      label="Regular"
+      theme={{
+        content: {
+          margin: 60,
+        },
+      }}
+    >
+      <Container />
+    </Preview>
+  </PreviewContainer>
+))
 
-  class Container extends React.Component {
-    constructor (props) {
-      super(props)
-      this.state = {
-        isSelected: false,
-      }
+class Container extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSelected: false,
     }
+  }
 
-    toggleSelected = () => {
-      this.setState(state => state.isSelected = !state.isSelected)
-    }
+  toggleSelected = () => {
+    this.setState(state => (state.isSelected = !state.isSelected))
+  }
 
-    handleChangeIsSelected = (isSelected) => {
-      action('onChangeIsSelected').call(null, isSelected)
-      this.setState(state => state.isSelected = isSelected)
-    }
+  handleChangeIsSelected = isSelected => {
+    action('onChangeIsSelected').call(null, isSelected)
+    this.setState(state => (state.isSelected = isSelected))
+  }
 
-    render () {
-      return (
-        <div>
-          <p><button onClick={this.toggleSelected}>Toggle selected</button></p>
-          <ComponentState
-            harnessCard={{
-              actions,
-              element,
-              harness: {
-                componentState: {
-                  name: 'With title',
-                },
-                alignment: {
-                  horizontal: 'Right',
-                },
-                theme: {
-                  patterns: {
-                    colors: {
-                      background: 'yellow',
-                    }
+  render() {
+    return (
+      <div>
+        <p><button onClick={this.toggleSelected}>Toggle selected</button></p>
+        <ComponentState
+          harnessCard={{
+            actions,
+            element,
+            harness: {
+              componentState: {
+                name: 'With title',
+              },
+              alignment: {
+                horizontal: 'Right',
+              },
+              theme: {
+                patterns: {
+                  colors: {
+                    background: 'yellow',
                   },
                 },
               },
-              isSelected: this.state.isSelected,
-            }}
-            onClickTitle={action('Click title')}
-            onChangeIsSelected={this.handleChangeIsSelected}
-          />
-        </div>
-      )
-    }
+            },
+            isSelected: this.state.isSelected,
+          }}
+          onClickTitle={action('Click title')}
+          onChangeIsSelected={this.handleChangeIsSelected}
+        />
+      </div>
+    )
   }
+}
