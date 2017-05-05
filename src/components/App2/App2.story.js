bafa2e918adcd6336bbc572fd846c13c05ab2153
Yaniv Tal <yanivtalmusic@gmail.com>
2017-05-05 15:35:43 -0700

    Work on App2

--- src/components/App2/App2.story.js
+++ src/components/App2/App2.story.js
@@ -3,6 +3,7 @@ import { storiesOf, action } from '@kadira/storybook'
 import { Colors } from '@workflo/styles'
 import App from './App2'
 import Components from '../Components'
+import ComponentStates from '../ComponentStates'
 import Properties from '../Properties'
 import Header from '../Header'
 import QuickAction from '../QuickAction'
@@ -27,7 +28,123 @@ import {
  * 6) Small vertical size
  */
 
-storiesOf('App2', module).add('App', () => <AppContainer />)
+storiesOf('App2', module)
+  .add('App', () => <AppContainer />)
+  .add('Live View (Large height)', () => (
+    <App
+      sections={{
+        header: {
+          element: (
+            <Header
+              title={{
+                value: 'Drawer',
+              }}
+              subtitle={{
+                value: 'Collapsed',
+              }}
+              onClickBack={() => {
+                this.setState({
+                  screen: 'component states',
+                })
+              }}
+            />
+          ),
+        },
+        centerLeft: {
+          element: (
+            <LivePreview
+              Component={MyComponent}
+              propMap={{}}
+              alignment={{
+                horizontal: 'Center',
+                vertical: 'Center',
+              }}
+              backgroundColor="cyan"
+            />
+          ),
+          layout: {
+            height: 700,
+          },
+        },
+        centerRight: {
+          element: (
+            <LiveEditor
+              componentTree={componentTree}
+              data={{ text: dataCode }}
+              actions={{ text: actionsCode }}
+              onChangeData={action('onChangeData')}
+              onChangeActions={action('onChangeActions')}
+            />
+          ),
+        },
+        bottom: {
+          element: <Properties properties={liveViewState.component.properties} />,
+        },
+      }}
+    />
+  ))
+  .add('Live View (Large width and height)', () => (
+    <App
+      sections={{
+        header: {
+          element: (
+            <Header
+              title={{
+                value: 'Drawer',
+              }}
+              subtitle={{
+                value: 'Collapsed',
+              }}
+              onClickBack={() => {
+                this.setState({
+                  screen: 'component states',
+                })
+              }}
+            />
+          ),
+        },
+        centerLeft: {
+          element: (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                flex: 1,
+              }}
+            >
+              <LivePreview
+                Component={MyComponent}
+                propMap={{}}
+                alignment={{
+                  horizontal: 'Center',
+                  vertical: 'Center',
+                }}
+                backgroundColor="cyan"
+              />
+              <LiveEditor
+                componentTree={componentTree}
+                data={{ text: dataCode }}
+                actions={{ text: actionsCode }}
+                onChangeData={action('onChangeData')}
+                onChangeActions={action('onChangeActions')}
+                theme={{
+                  liveEditor: {
+                    flex: '0 1 350px',
+                  },
+                }}
+              />
+            </div>
+          ),
+          layout: {
+            height: 700 + 350,
+          },
+        },
+        bottom: {
+          element: <Properties properties={liveViewState.component.properties} />,
+        },
+      }}
+    />
+  ))
 
 class AppContainer extends React.Component {
   constructor() {
@@ -40,6 +157,13 @@ class AppContainer extends React.Component {
 
   handleClickComponent = id => {
     this.setState({
+      screen: 'component states',
+      id,
+    })
+  }
+
+  handleClickState = id => {
+    this.setState({
       screen: 'live view',
       id,
     })
@@ -51,67 +175,119 @@ class AppContainer extends React.Component {
       case 'components':
         return (
           <App
-            backgroundColor={Colors.grey50}
-            layout={{
-              header: (
-                <Header
-                  title={{
-                    value: 'Components',
-                  }}
-                  subtitle={{
-                    value: 'All',
-                  }}
-                />
-              ),
-              centerLeft: (
-                <Components
-                  components={components}
-                  onClickComponent={this.handleClickComponent}
-                />
-              ),
+            backgroundColor={Colors.grey100}
+            sections={{
+              header: {
+                element: (
+                  <Header
+                    title={{
+                      value: 'Components',
+                    }}
+                    subtitle={{
+                      value: 'All',
+                    }}
+                  />
+                ),
+              },
+              filters: {
+                element: <div />,
+              },
+              centerLeft: {
+                element: (
+                  <Components
+                    components={components}
+                    onClickComponent={this.handleClickComponent}
+                  />
+                ),
+              },
+            }}
+          />
+        )
+      case 'component states':
+        return (
+          <App
+            backgroundColor={Colors.grey100}
+            sections={{
+              header: {
+                element: (
+                  <Header
+                    title={{
+                      value: 'Components',
+                    }}
+                    subtitle={{
+                      value: 'All',
+                    }}
+                    onClickBack={() => {
+                      this.setState({
+                        screen: 'components',
+                      })
+                    }}
+                  />
+                ),
+              },
+              filters: {
+                element: <div />,
+              },
+              centerLeft: {
+                element: (
+                  <ComponentStates
+                    harnessCards={stateCards}
+                    onClick={this.handleClickState}
+                    onChange={action('onChange')}
+                  />
+                ),
+              },
             }}
           />
         )
       case 'live view':
         return (
           <App
-            layout={{
-              header: (
-                <Header
-                  title={{
-                    value: 'Drawer',
-                  }}
-                  subtitle={{
-                    value: 'Collapsed',
-                  }}
-                  onClickBack={() => {
-                    this.setState({
-                      screen: 'components',
-                    })
-                  }}
-                />
-              ),
-              centerLeft: (
-                <LivePreview
-                  Component={MyComponent}
-                  propMap={{}}
-                  alignment={{
-                    horizontal: 'Center',
-                    vertical: 'Center',
-                  }}
-                  backgroundColor="cyan"
-                />
-              ),
-              centerRight: (
-                <LiveEditor
-                  componentTree={componentTree}
-                  data={{ text: dataCode }}
-                  actions={{ text: actionsCode }}
-                  onChangeData={action('onChangeData')}
-                  onChangeActions={action('onChangeActions')}
-                />
-              ),
-              bottom: <Properties properties={liveViewState.component.properties} />,
+            sections={{
+              header: {
+                element: (
+                  <Header
+                    title={{
+                      value: 'Drawer',
+                    }}
+                    subtitle={{
+                      value: 'Collapsed',
+                    }}
+                    onClickBack={() => {
+                      this.setState({
+                        screen: 'component states',
+                      })
+                    }}
+                  />
+                ),
+              },
+              centerLeft: {
+                element: (
+                  <LivePreview
+                    Component={MyComponent}
+                    propMap={{}}
+                    alignment={{
+                      horizontal: 'Center',
+                      vertical: 'Center',
+                    }}
+                    backgroundColor="cyan"
+                  />
+                ),
+              },
+              centerRight: {
+                element: (
+                  <LiveEditor
+                    componentTree={componentTree}
+                    data={{ text: dataCode }}
+                    actions={{ text: actionsCode }}
+                    onChangeData={action('onChangeData')}
+                    onChangeActions={action('onChangeActions')}
+                  />
+                ),
+              },
+              bottom: {
+                element: <Properties properties={liveViewState.component.properties} />,
+              },
             }}
           />
         )
@@ -136,4 +312,95 @@ const MyComponent = () => (
   >
     Hello
   </div>
+)
+
+const actions = [
+  <QuickAction
+    icon="delete"
+    input={{
+      type: 'Radio',
+      options: ['Left', 'Center', 'Right'],
+      value: 'Center',
+    }}
+    onChange={action('Alignment')}
+  />,
+  <QuickAction
+    icon="duplicate"
+    input={{
+      type: 'Radio',
+      options: ['Small', 'Base', 'Large'],
+      value: 'Base',
+    }}
+    onChange={action('Size')}
+  />,
+]
+
+const getCard = ({ name, alignment, color, size = 'Base', isSelected = false }) => ({
+  actions,
+  element,
+  harness: {
+    id: Math.random().toString(),
+    componentState: {
+      name,
+    },
+    alignment: {
+      horizontal: alignment,
+    },
+    size: {
+      horizontal: size,
+    },
+    theme: {
+      patterns: {
+        colors: {
+          background: color,
+        },
+      },
+    },
+  },
+  isSelected,
+  forceShowActions: false,
+})
+
+const stateCards = [
+  getCard({
+    name: 'With title',
+    alignment: 'Center',
+    color: 'yellow',
+  }),
+  getCard({
+    name: 'Another one',
+    alignment: 'Left',
+    color: 'cyan',
+    isSelected: true,
+  }),
+  getCard({
+    name: 'Full width align left',
+    alignment: 'Left',
+    color: 'cyan',
+    size: 'Large',
+  }),
+  getCard({
+    name: 'Full width align right',
+    alignment: 'Right',
+    color: 'yellow',
+    size: 'Large',
+  }),
+]
+
+const MockComponent = ({ children }) => (
+  <div
+    style={{
+      width: 100,
+      height: 100,
+      backgroundColor: 'magenta',
+    }}
+  >
+    {children}
+  </div>
+)
+
+const element = (
+  <MockComponent>
+    <div>Inner Text</div>
+  </MockComponent>
 )
---
+++ src/utils/sizes.js
@@ -0,0 +1,5 @@
+export const verticalSizes = {
+  Small: 200,
+  Base: 350,
+  Large: 700,
+}
