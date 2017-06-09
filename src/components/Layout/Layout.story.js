6e896a8c09d028f44dd4040b9ad7a69f2af05a30
Brandon Ramirez <brandon.ramirez@live.com>
2017-06-09 15:16:55 -0700

    Layout: Add show/hide functionality for Layout sections

--- src/components/Layout/Layout.story.js
+++ src/components/Layout/Layout.story.js
@@ -1,13 +1,17 @@
+/** @flow */
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { Colors } from '@workflo/styles'
 
+import Button from '../Button'
 import Layout from './'
 
 const fillParent = { flexGrow: 1 }
 
-const AppShell = ({ children }: { children: React.Element }) => (
+/** Renders Layout.App with placeholder content */
+const AppShell = ({ children, ...props }: { children?: React.Element<*> }) => (
   <Layout.App
+    {...props}
     theme={{
       container: {
         color: 'white',
@@ -28,8 +32,10 @@ const AppShell = ({ children }: { children: React.Element }) => (
   </Layout.App>
 )
 
-const LiveViewShell = ({ children }: { children: React.Element }) => (
+/** Renders Layout.LiveView with placeholder content */
+const LiveViewShell = ({ children, ...props }: { children?: React.Element<*> }) => (
   <Layout.LiveView
+    {...props}
     rightPanel={
       <div style={{ backgroundColor: Colors.red700, ...fillParent }}>Right Panel</div>
     }
@@ -50,6 +56,49 @@ const ContentPlaceholder = () => (
   </div>
 )
 
+/**
+ * Container for showing how sections can be toggled with the Layout.App and
+ * Layout.LiveView components.
+ */
+class LiveViewToggleContainer extends React.Component {
+  Props: {}
+  State: {
+    chrome: number,
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      chrome: 0,
+    }
+  }
+
+  handleToggle = props => {
+    this.setState(prevState => ({ chrome: (prevState.chrome + 1) % 3 }))
+  }
+
+  render(props) {
+    return (
+      <AppShell
+        showLeftNav={this.state.chrome === 0}
+        showHeader={this.state.chrome === 0}
+      >
+        <LiveViewShell
+          showBottomPanel={this.state.chrome !== 2}
+          showLeftPanel={this.state.chrome !== 2}
+          showRightPanel={this.state.chrome !== 2}
+        >
+          <div style={{ ...fillParent }}>
+            <Button onClick={this.handleToggle} style={{ height: 50, width: 150 }}>
+              Toggle Chrome
+            </Button>
+            Mode: {this.state.chrome}
+          </div>
+        </LiveViewShell>
+      </AppShell>
+    )
+  }
+}
+
 storiesOf('Layout', module)
   .add('App', () => <AppShell><ContentPlaceholder /></AppShell>)
   .add('LiveView', () => (
@@ -59,3 +108,4 @@ storiesOf('Layout', module)
       </LiveViewShell>
     </AppShell>
   ))
+  .add('Toggle LiveView Chrome', () => <LiveViewToggleContainer />)
