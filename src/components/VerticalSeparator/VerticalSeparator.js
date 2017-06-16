03f32c902c8fcc0403ea1265f4b4e6a4f76df8e0
Yaniv Tal <yanivtalmusic@gmail.com>
2017-06-15 20:57:59 -0700

    Add VerticalSeparator component

---
+++ src/components/VerticalSeparator/VerticalSeparator.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Theme from 'js-theme'
+import { Colors, Spacing } from '@workflo/styles'
+
+type PropsT = {
+  /** How high to make the separator in pixels  */
+  height: number,
+  /** A valid CSS color for the separator */
+  color: string,
+  /** How many pixels to leave on the left */
+  marginLeft: number,
+  /** How many pixels to leave on the right */
+  marginRight: number,
+}
+
+const defaultProps = {
+  height: 30,
+  color: Colors.grey500,
+  marginLeft: Spacing.small,
+  marginRight: Spacing.small,
+}
+
+const VerticalSeparator = ({ theme }: PropsT) => <div {...theme.separator} />
+VerticalSeparator.defaultProps = defaultProps
+
+const defaultTheme = ({ color, marginLeft, marginRight }) => {
+  return {
+    separator: {
+      flex: '0 1 auto',
+      borderLeftWidth: 1,
+      borderLeftStyle: 'solid',
+      borderLeftColor: color,
+      height: 40, // Make line-size height
+      marginLeft,
+      marginRight,
+    },
+  }
+}
+
+export default Theme('VerticalSeparator', defaultTheme)(VerticalSeparator)
