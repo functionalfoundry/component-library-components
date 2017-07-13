28b409bf59dcf7f2f6f2cf0e49a25871308c5807
Brandon Ramirez <brandon.ramirez@live.com>
2017-07-12 19:49:14 -0700

    ComponentTreeEditor: Hook up PropValueChooser and onChange events

--- src/components/ComponentTreeEditor/components/PropRenderer.js
+++ src/components/ComponentTreeEditor/components/PropRenderer.js
@@ -5,26 +5,41 @@ import Theme from 'js-theme'
 import { Colors } from '@workflo/styles'
 
 import type { CompletionOptionT } from '../../../types/Completion'
-import type { Prop } from '../../../modules/ComponentTree'
+import type { ComponentTree, Prop } from '../../../modules/ComponentTree'
 
 import Line from './Line'
 import PropValueRenderer from './PropValueRenderer'
 
 type Props = {
   completionOptions: Array<CompletionOptionT>,
+  componentTree: ComponentTree,
+  onChange: Function,
+  onChangePropValue: Function,
   propNode: Prop,
   indentLevel: number,
   theme: Object,
 }
 
-const PropRenderer = ({ completionOptions, indentLevel, propNode, theme }: Props) => {
+const PropRenderer = ({
+  completionOptions,
+  componentTree,
+  indentLevel,
+  onChange,
+  onChangePropValue,
+  propNode,
+  theme,
+}: Props) => {
   return propNode.get('value') && propNode.get('name')
     ? <Line indentLevel={indentLevel}>
         <span {...theme.propName}>{propNode.get('name')}</span>
         {'='}
         <PropValueRenderer
           completionOptions={completionOptions}
+          componentTree={componentTree}
+          onChange={onChange}
+          onChangePropValue={onChangePropValue}
           propValueNode={propNode.get('value')}
+          propNode={propNode}
         />
       </Line>
     : null
