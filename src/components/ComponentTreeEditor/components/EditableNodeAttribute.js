99467bc67cadedfcf7eab3d561b56483f367f8c9
Jannis Pohlmann <contact@jannispohlmann.de>
2017-08-01 15:08:44 +0200

    EditableNodeAttribute: Remove unused props, make some optional

--- src/components/ComponentTreeEditor/components/EditableNodeAttribute.js
+++ src/components/ComponentTreeEditor/components/EditableNodeAttribute.js
@@ -6,13 +6,12 @@ import { Path } from '../../../modules/ComponentTree'
 import type { InteractionStateT } from '../types'
 import EditableField from './EditableField'
 
-type Props = {
-  focusNextNode: Function,
+type PropsT = {
   /**
    * An optional function to be applied to value before rendering. It is passed `value`
    * and `isFocused` as named parameters.
    */
-  formatValue: Function,
+  formatValue?: Function,
   /** Contains information as to which node in the componentTree should be focused */
   interactionState: InteractionStateT,
   onBlur: Function,
@@ -30,7 +29,7 @@ type Props = {
    */
   onFocusPrevious: Function,
   options?: Array<any>,
-  optionRenderer: Function,
+  optionRenderer?: Function,
   /** The ID of the componentTree node being modififed */
   nodeId: string,
   /** The path of the attribute being modified within the componentTree node (may be nested) */
@@ -40,9 +39,9 @@ type Props = {
 }
 
 class EditableNodeAttribute extends PureComponent {
-  props: Props
+  props: PropsT
 
-  computeIsFocused = ({ interactionState, path }: Props) =>
+  computeIsFocused = ({ interactionState, path }: PropsT) =>
     is(interactionState.focusedNodePath, path)
 
   handleBlur = () => this.props.onBlur && this.props.onBlur(this.props.path)
