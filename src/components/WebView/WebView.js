e421e43b49c62e260508073eeb05d176b1edac73
Jannis Pohlmann <contact@jannispohlmann.de>
2017-08-30 12:51:51 +0200

    WebView: WIP: Add a new WebView component

---
+++ src/components/WebView/WebView.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ComponentTree from '../../modules/ComponentTree'
+const ElectronWebView = require('react-electron-web-view')
+
+/**
+ * Props
+ */
+
+type BundlesT = Object<string, string>
+
+type PropsT = {
+  /** The component tree to render */
+  tree: ComponentTree,
+  /** A chunk with common JS modules for components from the same repo */
+  commonsChunk: string,
+  /** Map from tree component IDs to bundle strings */
+  bundles: BundlesT,
+}
+
+/**
+ * State
+ */
+
+type StateT = {}
+
+/**
+ * WebView component
+ */
+
+class WebView extends React.Component {
+  props: PropsT
+  state: StateT
+
+  render() {
+    return (
+      <ElectronWebView
+        src="data:text/html,<html><body>Web view body</body></html>"
+        autosize={false}
+        nodeintegration={false}
+        plugins={false}
+        preload="./boilerplate.js"
+      />
+    )
+  }
+}
+
+export default WebView
---
+++ src/components/WebView/boilerplate.js
@@ -0,0 +1,1 @@
+console.warn('Hello from boilerplate.js')
