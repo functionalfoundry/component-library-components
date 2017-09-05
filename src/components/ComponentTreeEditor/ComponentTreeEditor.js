functionalfoundry/component-library-components

commit 8f60332b58f5e7ab66f35cbc5a6d690297f23b2f

ComponentTreeEditor: Implement TextNode component and functionality

src/components/ComponentTreeEditor/ComponentTreeEditor.js
@@ -145,7 +145,8 @@ class ComponentTreeEditor extends React.Component {
         }
         if (
           (node.nodeType === 'prop' && !node.get('name')) ||
-          (node.nodeType === 'component' && !node.get('name'))
+          (node.nodeType === 'component' && !node.get('name')) ||
+          (node.nodeType === 'text' && !node.get('value'))
         ) {
           return ctx.set('data', ctx.data.push(node.id))
         }
@@ -322,7 +323,12 @@ class ComponentTreeEditor extends React.Component {
     const newTree = Helpers.insertNodeAtPath(componentTree, insertionPath, newNode)
 
     this.updateComponentTree(newTree, () => {
-      this.focusNodeAttribute(insertionPath.push('name'))
+      if (newNode.nodeType === 'component') {
+        this.focusNodeAttribute(insertionPath.push('name'))
+      }
+      if (newNode.nodeType === 'text') {
+        this.focusNodeAttribute(insertionPath.push('value'))
+      }
     })
 
     const parentPath = insertionPath.pop().pop()

src/components/ComponentTreeEditor/components/ComponentRenderer.js
@@ -12,6 +12,7 @@ import AddNodeButton from './AddNodeButton'
 import PropRenderer from './PropRenderer'
 import Line from './Line'
 import EditableNodeAttribute from './EditableNodeAttribute'
+import TextNode from './TextNode'
 
 type Props = {
   completionData: CompletionDataT,
@@ -174,28 +175,42 @@ const ComponentRenderer = ({
           })}
         </Line>}
       {childComponents
-        .map(
-          (child, index) =>
-            child.nodeType === 'component'
-              ? <ThemedComponentRenderer
-                  key={child.get('id')}
-                  completionData={completionData}
-                  componentNode={child}
-                  componentTree={componentTree}
-                  indentLevel={indentLevel + 1}
-                  interactionState={interactionState}
-                  onChangeNode={onChangeNode}
-                  onChangePropName={onChangePropName}
-                  onChangePropValue={onChangePropValue}
-                  onSelectComponent={onSelectComponent}
-                  onInsertNode={onInsertNode}
-                  onBlur={onBlur}
-                  onFocus={onFocus}
-                  onFocusNext={onFocusNext}
-                  onFocusPrevious={onFocusPrevious}
-                />
-              : child.nodeType
-        )
+        .map((child, index) => {
+          if (child.nodeType === 'text') {
+            return (
+              <TextNode
+                key={child.get('id')}
+                indentLevel={indentLevel + 1}
+                interactionState={interactionState}
+                onBlur={onBlur}
+                onChangeNode={onChangeNode}
+                onFocus={onFocus}
+                onFocusNext={onFocusNext}
+                onFocusPrevious={onFocusPrevious}
+                textNode={child}
+              />
+            )
+          }
+          return child.nodeType === 'component'
+            ? <ThemedComponentRenderer
+                key={child.get('id')}
+                completionData={completionData}
+                componentNode={child}
+                componentTree={componentTree}
+                indentLevel={indentLevel + 1}
+                interactionState={interactionState}
+                onChangeNode={onChangeNode}
+                onChangePropName={onChangePropName}
+                onChangePropValue={onChangePropValue}
+                onSelectComponent={onSelectComponent}
+                onInsertNode={onInsertNode}
+                onBlur={onBlur}
+                onFocus={onFocus}
+                onFocusNext={onFocusNext}
+                onFocusPrevious={onFocusPrevious}
+              />
+            : child.nodeType
+        })
         .toArray()}
       {childComponents.count() > 0 &&
         <Line indentLevel={indentLevel}>

src/components/ComponentTreeEditor/components/EditableField.js
@@ -27,12 +27,14 @@ type ContainerPropsT = {
    * and `isFocused` as named parameters.
    */
   formatValue: Function,
-  freeformRenderer: Function,
+  freeformRenderer?: Function,
   /**
    * Indicates whether the editableField focused. Is merged in with local isFocused state
    * for faster local updates when the focus is triggered from the "inside".
    */
   isFocused: boolean,
+  /** Indicates whether the text input should expand to multiple lines */
+  multipleLines?: boolean,
   onBlur: Function,
   onChange: Function,
   /**
@@ -163,6 +165,15 @@ class EditableFieldContainer extends React.Component {
       event.preventDefault()
     }
 
+    /**
+     * Focus next if enter is hit. Note that this logic is handled in handleSelect when
+     * OptionChooser is present (it will swallow the enter keyboard event).
+     */
+    if (event.keyCode === 13) {
+      onFocusNext()
+      this.editableText.refs.wrappedInstance.blur()
+    }
+
     /** Use ESC to cancel focus */
     if (event.keyCode === 27) {
       this.editableText.refs.wrappedInstance.blur()
@@ -204,6 +215,7 @@ class EditableFieldContainer extends React.Component {
       accessOption,
       formatValue,
       freeformRenderer,
+      multipleLines,
       options,
       optionRenderer,
       value,
@@ -214,6 +226,7 @@ class EditableFieldContainer extends React.Component {
         <EditableField
           editableTextRef={this.saveRefToEditableText}
           isFocused={this.state.isFocused}
+          multipleLines={multipleLines}
           onChange={this.handleChange}
           onStartEdit={this.handleStartEdit}
           onStopEdit={this.handleStopEdit}
@@ -256,6 +269,7 @@ class EditableFieldContainer extends React.Component {
 type Props = {
   editableTextRef: Function,
   isFocused: boolean,
+  multipleLines: boolean,
   onChange: Function,
   onStartEdit: Function,
   onStopEdit: Function,
@@ -272,11 +286,19 @@ class EditableField extends React.Component {
     return true
   }
   render() {
-    const { editableTextRef, onChange, onStartEdit, onStopEdit, value } = this.props
+    const {
+      editableTextRef,
+      multipleLines,
+      onChange,
+      onStartEdit,
+      onStopEdit,
+      value,
+    } = this.props
     return (
       <span>
         <EditableText
           inline
+          multipleLines={multipleLines}
           onChange={onChange}
           onStartEdit={onStartEdit}
           onStopEdit={onStopEdit}

src/components/ComponentTreeEditor/components/EditableNodeAttribute.js
@@ -35,6 +35,8 @@ type PropsT = {
   onFocusPrevious: Function,
   options?: Array<any>,
   optionRenderer?: Function,
+  /** Indicates whether the value can extend to multiple lines */
+  multipleLines?: boolean,
   /** The ID of the componentTree node being modififed */
   nodeId: string,
   /** The path of the attribute being modified within the componentTree node (may be nested) */

src/components/ComponentTreeEditor/components/TextNode.js
@@ -1,0 +1,53 @@
+import React from 'react'
+import type { InteractionStateT } from '../types'
+import { TextNode as TextNodeT } from '../../../modules/ComponentTree'
+import EditableNodeAttribute from './EditableNodeAttribute'
+import Line from './Line'
+
+const displayTextValue = ({ value, isFocused }) => (isFocused ? value : `{'${value}'}`)
+
+type Props = {
+  indentLevel: number,
+  interactionState: InteractionStateT,
+  onBlur: Function,
+  onChangeNode: Function,
+  onFocus: Function,
+  onFocusNext: Function,
+  onFocusPrevious: Function,
+  textNode: TextNodeT,
+}
+
+class TextNode extends React.Component {
+  props: Props
+  render() {
+    const {
+      indentLevel,
+      interactionState,
+      onBlur,
+      onChangeNode,
+      onFocus,
+      onFocusNext,
+      onFocusPrevious,
+      textNode,
+    } = this.props
+    return (
+      <Line indentLevel={indentLevel}>
+        <EditableNodeAttribute
+          formatValue={displayTextValue}
+          interactionState={interactionState}
+          nodeId={textNode.id}
+          onBlur={onBlur}
+          onChangeNode={onChangeNode}
+          onFocus={onFocus}
+          onFocusNext={onFocusNext}
+          onFocusPrevious={onFocusPrevious}
+          path={textNode.path.push('value')}
+          value={textNode.value}
+        />
+        <span>&nbsp;</span>
+      </Line>
+    )
+  }
+}
+
+export default TextNode

src/components/ComponentTreeEditor/utils/generateTraversalMap.js
@@ -2,6 +2,7 @@ import {
   type ComponentTree,
   type Component,
   type Prop,
+  type TextNode,
   Path,
 } from '../../../modules/ComponentTree'
 import { List, Map, Record } from 'immutable'
@@ -31,6 +32,11 @@ const generateTraversalMap = (componentTree: ComponentTree): TraversalMapT => {
     }
   }
 
+  const processTextNode = (textNode: TextNode) => {
+    const path = textNode.get('path')
+    traversalList.push(NodePointer({ path: path.push('value'), type: 'text' }))
+  }
+
   const processComponent = (componentNode: Component) => {
     const path = componentNode.get('path')
     const props = componentNode.get('props')
@@ -47,6 +53,9 @@ const generateTraversalMap = (componentTree: ComponentTree): TraversalMapT => {
     }
     traversalList.push(NodePointer({ path: path.push('add-button'), type: null }))
     componentNode.get('children').forEach(child => {
+      if (child.nodeType === 'text') {
+        processTextNode(child)
+      }
       if (child.nodeType === 'component') {
         processComponent(child)
       }

src/modules/ComponentTree/Helpers.js
@@ -69,6 +69,8 @@ const traverse = (tree: ComponentTree, data: any, visitor: Function): TraverseRe
       .setIn(['node', ...relativePath], childCtx.node)
   }
 
+  const traverseTextNode = (ctx: TraverseContext): TraverseContext => ctx.visitor(ctx)
+
   /**
    * Traverses a component node, its name, its props and its children
    * and merges the result into the parent context.
@@ -88,6 +90,9 @@ const traverse = (tree: ComponentTree, data: any, visitor: Function): TraverseRe
     // Traverse the component's children
     if (!ctx.node.children.isEmpty()) {
       ctx = ctx.node.children.reduce((ctx, child, index) => {
+        if (child.nodeType === 'text') {
+          return traverseChildNode(ctx, ['children', index], child, traverseTextNode)
+        }
         if (child.nodeType === 'component') {
           return traverseChildNode(ctx, ['children', index], child, traverseComponent)
         }

commit 74513491267553bf897e6faff2a7d78d1d4631be

ComponentTreeEditor: Fix focusing the prop name after adding a prop
The issue was introduced by a change in 8f60332, after which only
new components and new text nodes were focused but not the name
nodes of new props.

src/components/ComponentTreeEditor/ComponentTreeEditor.js
@@ -323,7 +323,7 @@ class ComponentTreeEditor extends React.Component {
     const newTree = Helpers.insertNodeAtPath(componentTree, insertionPath, newNode)
 
     this.updateComponentTree(newTree, () => {
-      if (newNode.nodeType === 'component') {
+      if (newNode.nodeType === 'component' || newNode.nodeType === 'prop') {
         this.focusNodeAttribute(insertionPath.push('name'))
       }
       if (newNode.nodeType === 'text') {