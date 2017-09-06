398b87068dee71ad9aa3e4a9c1ae50356eb40e05
Brandon Ramirez <brandon.ramirez@live.com>
2017-09-06 12:23:48 -0700

    LivePreview: Fix fullscreen behavior

--- src/components/Layout/components/Column.js
+++ src/components/Layout/components/Column.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 /** Helper component for rendering flexbox column w/ semantically meaninful markup */
-const Column = ({ children, ...props }: { children: React.Element }) => (
-  <div {...props} style={{ display: 'flex', flexDirection: 'column' }}>{children}</div>
-)
+const Column = ({
+  children,
+  style,
+  ...props
+}: {
+  children: React.Element,
+  style: Object,
+}) =>
+  <div {...props} style={{ display: 'flex', flexDirection: 'column', ...style }}>
+    {children}
+  </div>
 
 export default Column
